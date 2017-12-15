89d0596089695060943f1fc07d74755d1c5cec32
ghstahl@gmail.com <ghstahl@gmail.com>
2017-12-15 10:21:55 -0800

    initial

---
+++ src/MEF/Hello.Core/Startup.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Hello.Core
+{
+    public class Startup
+    {
+        public async Task<object> Invoke(object input)
+        {
+            return "Hello from dot net core 2";
+        }
+    }
+}
