17059a41c49df51d0d7b5be0b54a07579ae2954c
ghstahl@gmail.com <ghstahl@gmail.com>
2017-12-17 11:53:10 -0800

    Can't load when I have a reference to an external library

--- src/MEF/Hello.Core/Startup.cs
+++ src/MEF/Hello.Core/Startup.cs
@@ -1,13 +1,30 @@
-﻿using System;
+﻿using System.IO;
+using System.Reflection;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Some.Library;
+
 
 namespace Hello.Core
 {
     public class Startup
     {
+       // public CommandCompositionHelper CommandCompositionHelper { get; set; }
+
+        public Startup()
+        {
+
+        }
         public async Task<object> Invoke(object input)
         {
-            return "Hello from dot net core 2";
+            // this works which make me think that Newtonsoft.Json.dll is loadable either by being somewhere on disk or 
+            // because of the nature of the assembly.
+            var dd = JsonConvert.SerializeObject("Hello from dot net core 2");
+            var pp = new SomeClass().GetDateTimeNow();
+
+
+
+            return dd + pp;
         }
     }
 }
